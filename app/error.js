a4c15b682341ba669f93dc568737bb4a60be059f
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2024-12-17 06:34:01 +0530

    Update error.js
    - Modified page styles

--- app/error.js
+++ app/error.js
@@ -8,15 +8,17 @@ export default function Error({ error, reset }) {
   }, [error])
  
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h2>Something went wrong!</h2>
+        <button
+          onClick={
+            () => reset()
+          }
+        >
+          Try again
+        </button>
+      </main>
     </div>
   )
 }
