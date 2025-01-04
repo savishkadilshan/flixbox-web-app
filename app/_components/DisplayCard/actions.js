32fdbbd8ca851fa100b86e0679b43e1a578c21f1
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2025-01-04 07:27:58 +0530

    Implement game saving UI change
    - After the click 'Save' button in the game card, Save button state will change and UI also changing.
    - When component is mount check whether game is save or not. According to that UI will change.

--- app/_components/DisplayCard/actions.js
+++ app/_components/DisplayCard/actions.js
@@ -1,41 +1,88 @@
-'use server'
+"use server"
 
 import { decrypt } from "@/app/_utils/session/session";
 import { cookies } from "next/headers";
-import { MongoClient, ObjectId, ServerApiVersion } from "mongodb"
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 
-export default async function saveGameInUserMyList(gameId) {
-    const cookie = (await cookies()).get('session')?.value;
+export async function saveAndUpdateMyList(gameId) {
+  const cookie = (await cookies()).get("session")?.value;
+  const session = await decrypt(cookie);
+
+  const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  try {
+    await client.connect();
+    const database = client.db("FlixBox");
+    const collection = database.collection("my-list");
+
+    const userGameList = await collection.findOne({
+      _id: session?.userId.userEmail,
+    });
+    const isGameStored = userGameList?.games.some(
+      (game) => game.gameId.equals(new ObjectId(gameId))
+    );
+
+    try {
+      if (isGameStored) {
+        // Remove the game from list
+        await collection.updateOne(
+          { _id: session?.userId.userEmail },
+          { $pull: { games: { gameId: new ObjectId(gameId) } } }
+        );
+      } else {
+        // Add new game to the list
+        await collection.findOneAndUpdate(
+          { _id: session?.userId.userEmail },
+          {
+            $push: {
+              games: {
+                gameId: new ObjectId(gameId),
+              },
+            },
+          },
+          { upsert: true }
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } catch (error) {
+    console.log("There is an issue while connecting to FlixBox database. Error: ", error);
+  } finally {
+    await client.close();
+  }
+}
+
+export async function checkGameIsSaved(gameId) {
+    const cookie = (await cookies()).get("session")?.value;
     const session = await decrypt(cookie);
 
     const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
     const client = new MongoClient(uri, {
         serverApi: {
-          version: ServerApiVersion.v1,
-          strict: true,
-          deprecationErrors: true,
-        }
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+        },
     });
-    console.log("User: ", session?.userId.userEmail);
-    console.log("Game ID: ", gameId)
 
     try {
         await client.connect();
-        const database = client.db('FlixBox');
-        const collection = database.collection('my-list');
+        const database = client.db("FlixBox");
+        const collection = database.collection("my-list");
 
         try {
-            await collection.findOneAndUpdate(
-                { _id: session?.userId.userEmail },
-                {
-                    $push: {
-                        games: {
-                            gameId: new ObjectId(gameId)
-                        }
-                    }
-                },
-                { returnDocument: "after", upsert: true }
+            const game = await collection.findOne(
+                { _id: session?.userId.userEmail, games: { gameId: new ObjectId(gameId) } }
             );
+            return !!game;
         } catch (error) {
             console.log(error);
         }
