758b69dd0aa63174a4b8bc14aae0d3eb7b4cb70f
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2025-01-14 09:30:01 +0530

    Add sign up redirect logic
    - Previously when user sign up, user not redirect to home page. User needs to again login to access home page.
    - Now updated sign up logic when user sign up redirect to the home page.

--- app/_components/SignupForm/SignupForm.jsx
+++ app/_components/SignupForm/SignupForm.jsx
@@ -1,15 +1,30 @@
 'use client'
 
+import { useRouter } from "next/navigation";
+import { useActionState, useEffect } from "react";
 import signup from "./actions";
 
 export default function SignUpForm() {
+  const router = useRouter()
+  const [state, action] = useActionState(signup, {
+    success: false,
+    redirectUrl: null,
+    error: null
+  })
+
+  useEffect(() => {
+    if(state.success) {
+      router.push(state.redirectUrl)
+    }
+  }, [state, router])
+
     return (
       <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-orange-50 to-orange-100 p-4">
         <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-6">
           <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
             Create Your Account
           </h2>
-          <form action={signup}>
+          <form action={action}>
             <div className="mb-4">
               <label
                 htmlFor="name"
--- app/_components/SignupForm/actions.js
+++ app/_components/SignupForm/actions.js
@@ -1,8 +1,9 @@
 'use server'
 
 import { getDbClient } from "@/app/_utils/mongodb/mongoClient";
+import { createSession } from "@/app/_utils/session/session";
 
-export default async function signup(formData) {
+export default async function signup(prevState, formData) {
     const bcrypt = require('bcrypt');
 
     try {
@@ -19,15 +20,23 @@ export default async function signup(formData) {
         const hashedPassword = await bcrypt.hash(validatedFields.userPassword, 10);
 
         try {
-            await collection.insertOne({
+            const newUser = await collection.insertOne({
                 "userName": validatedFields.userName,
                 "userEmail": validatedFields.userEmail,
                 "userPassword": hashedPassword
             });
+
+            if (newUser) {
+                await createSession(validatedFields.userEmail, "normal-user");
+                return {
+                    success: true,
+                    redirectUrl: '/'
+                }
+            }
         } catch (error) {
             console.log(error);
         }
     } catch (error) {
-        console.log("There is an issue while connecting to FlixBox database. Error: ", error);
+        console.log("There is an error: ", error);
     }
 }
